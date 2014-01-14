72ebdb2758a1d58c9d63772b14aa2ac63bf3f9f3
App Academy Student <student@appacademy.io>
2014-01-13 17:54:41 -0800

    Adds user list with live update.

--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -7,40 +7,52 @@ socket.on("connect", function(){
 var chat_room = new ChatModule.Chat(socket);
 
 $(function(){
-  var chat_room_div = $('#chat_room');
-
-  var chat_submit = $('#submit_chat');
+  var $chat_room_div = $('#chat_room');
+  var $user_list_div = $('#user_list');
+  var $chat_submit = $('#submit_chat');
+  var $user_ul = $('#user_ul')
 
   $('#chat_form').submit(function(event){
     event.preventDefault();
-    chat_room.sendMessage($('#chat_text').val());
+    $chat_room.sendMessage($('#chat_text').val());
   });
 
   socket.on("new_message", function(data){
-    console.log(data);
-    chat_room_div.append(data['nickname'] + ": ");
-    chat_room_div.append(data['message']);
-    chat_room_div.append('<br>');
+    $chat_room_div.append(data['nickname'] + ": ");
+    $chat_room_div.append(data['message']);
+    $chat_room_div.append('<br>');
   });
 
   socket.on("chat_message", function(data){
-    console.log(data);
-    chat_room_div.append(data['message']);
-    chat_room_div.append('<br>');
+    $chat_room_div.append(data['message']);
+    $chat_room_div.append('<br>');
   });
 
+  socket.on("user_list", function(data){
+    $user_ul.html('');
+    var nicks = data['nicknames'];
+    for (attr in nicks) {
+      if (!nicks.hasOwnProperty(attr)) {
+        continue
+      }
+      var $newLi = $('<li>');
+      $newLi.html(nicks[attr]);
+      $user_ul.append($newLi);
+    }
+
+  })
+
   socket.on("announcement", function(data){
-    console.log(data);
-    chat_room_div.append(data['message']);
-    chat_room_div.append('<br>');
+    $chat_room_div.append(data['message']);
+    $chat_room_div.append('<br>');
   });
 
   socket.on('nicknameChangeResult', function(data){
     console.log(data);
     if (data['success']) {
     } else {
-      chat_room_div.append("<span id='nickname_error'>" + data['message'] + "</span>");
-      chat_room_div.append('<br>');
+      $chat_room_div.append("<span id='nickname_error'>" + data['message'] + "</span>");
+      $chat_room_div.append('<br>');
     }
   })
 
