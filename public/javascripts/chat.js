89fa26b00376ceb2ad3822f32b7e08c59c92a539
App Academy Student <student@appacademy.io>
2014-01-13 18:08:36 -0800

    Fixes typo

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -6,13 +6,19 @@
   };
 
   Chat.prototype.sendMessage = function (message) {
+    // var command = message.match(/^\//);
     var res = message.match(/\/nick/);
-
-    if (res){
+    // if (command){
+    //   processCommand(message.slice(1))
+    if(res){
       socket.emit("nicknameChangeRequest", message.slice(6));
     } else {
       socket.emit("user_submit", message);
     }
+  };
+
+  Chat.prototype.processCommand = function (command) {
+
   };
 
 })(this);
